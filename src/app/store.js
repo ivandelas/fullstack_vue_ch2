9f129f0b3af5f912e7b8261adc4eee7ddf96bf36
Ivan Delaš <ivandelas@gmail.com>
2018-09-26 06:29:50 +0200

    deleteEvent

--- src/app/store.js
+++ src/app/store.js
@@ -42,4 +42,13 @@ export const store = {
       event => event.details === eventDetails
     );
   },
+  deleteEvent (dayId, eventDetails) {
+    const dayObj = this.state.seedData.find(
+      day => day.id === dayId
+    );
+    const eventIndexToRemove = dayObj.events.findIndex(
+      event => event.details === eventDetails
+    );
+    dayObj.events.splice(eventIndexToRemove, 1);
+  },
 }
